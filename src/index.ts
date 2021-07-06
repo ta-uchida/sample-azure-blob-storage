b2131fa7d60291c678cc7cda52614423d84adbf6
Takayoshi Uchida <takayoshi-uchida@mail.nissan.co.jp>
2021-07-07 01:13:04 +0900

    add upload and download sample

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,16 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import { GetContainerNames } from "./sample/getContainerNames";
+import { OperateBlob } from "./sample/operateBlob";
 
 (async function main() {
-  let i = 1;
-  let containers = new GetContainerNames().get();
-  for await (const container of containers) {
-    console.log(`Container ${i++}: ${container.name}`);
-  }
-})();
+  // let i = 1;
+  // let containers = new GetContainerNames().get();
+  // for await (const container of containers) {
+  //   console.log(`Container ${i++}: ${container.name}`);
+  // }
+
+  const operateBlob = new OperateBlob();
+  const uploadedFileName = await operateBlob.upload();
+  await operateBlob.download(uploadedFileName);
+})().catch(console.error);
---
+++ src/sample/operateBlob.ts
@@ -0,0 +1,52 @@
+import { BlobServiceClient, ContainerClient } from "@azure/storage-blob";
+import { blobService } from "../blobService";
+
+export class OperateBlob {
+  readonly #service: BlobServiceClient;
+  readonly #containerName = process.env.AZURE_BLOB_CONTAINER_NAME || '';
+  readonly #containerClient: ContainerClient;
+
+  constructor() {
+    this.#containerName = process.env.AZURE_BLOB_CONTAINER_NAME || '';
+    if (!this.#containerName) {
+      throw new Error('Not found Azure Blob target container name');
+    }
+    this.#service = blobService.getServiceInstance();
+    this.#containerClient = this.#service.getContainerClient(this.#containerName);
+  }
+
+  async upload(): Promise<string> {
+    const content = "Hello world!";
+    const blobName = "newblob" + new Date().getTime();
+    const blockBlobClient = this.#containerClient.getBlockBlobClient(blobName);
+    const uploadBlobResponse = await blockBlobClient.upload(content, content.length);
+    console.log(`Upload block blob ${blobName} successfully`, uploadBlobResponse.requestId);
+    // e.g. Upload block blob newblob1625586283552 successfully e7a8282b-401e-0181-1b7d-72d2d4000000
+    return Promise.resolve(blobName);
+  }
+
+  async download(blobName: string) {
+    const blobClient = this.#containerClient.getBlobClient(blobName);
+
+    // Get blob content from position 0 to the end
+    // In Node.js, get downloaded data by accessing downloadBlockBlobResponse.readableStreamBody
+    const downloadBlockBlobResponse = await blobClient.download();
+    const buffer = await this.#streamToBuffer(downloadBlockBlobResponse.readableStreamBody);
+    const downloaded = buffer.toString();
+    console.log("Downloaded blob content:", downloaded);
+  }
+
+  // [Node.js only] A helper method used to read a Node.js readable stream into a Buffer
+  async #streamToBuffer(readableStream: NodeJS.ReadableStream | undefined): Promise<Buffer> {
+    return new Promise((resolve, reject) => {
+      const chunks: Buffer[] = [];
+      readableStream?.on("data", (data) => {
+        chunks.push(data instanceof Buffer ? data : Buffer.from(data));
+      });
+      readableStream?.on("end", () => {
+        resolve(Buffer.concat(chunks));
+      });
+      readableStream?.on("error", reject);
+    });
+  }
+}
